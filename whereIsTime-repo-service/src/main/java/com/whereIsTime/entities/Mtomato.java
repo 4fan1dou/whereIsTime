01908d2309a12d7d1e67d0065e52be2eb479f5c2
Junjie Huang <349373001@qq.com>
2016-06-26 23:08:36 +0800

    Revert "Revert "Feature server""

--- whereIsTime-repo-service/src/main/java/com/whereIsTime/entities/Mtomato.java
+++ whereIsTime-repo-service/src/main/java/com/whereIsTime/entities/Mtomato.java
@@ -1,6 +1,7 @@
 package com.whereIsTime.entities;
 
 import java.util.Date;
+import java.text.DateFormat;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -14,26 +15,44 @@ import lombok.Setter;
 @Table(name = "Mtomato")
 @Entity
 public class Mtomato extends baseEntity {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -8474007738347800829L;
-
-	public Mtomato() {super();}
 	
+	public static enum feedback {great, normal, bad};
+
+	public Mtomato() {
+		super();
+	}
+
 	@Column(nullable = false)
 	private @Getter @Setter Integer nt;
 
+	public void setBeginTime(Date t) {
+		this.day = DateFormat.getDateInstance().format(t);
+		this.beginTime = t;
+	}
+	
 	@Column(nullable = false)
-	private @Getter @Setter Date beginTime;
+	private @Getter Date beginTime;
+
+	@Column(nullable = false)
+	private @Getter String day;
 
 	@Column(nullable = false)
 	private @Getter @Setter Date endTime;
-	
+
 	@Column(nullable = false)
-	private @Getter @Setter User.Status feedBack = User.Status.level1;
-	
-	@ManyToOne(cascade = CascadeType.ALL)
+	private @Getter @Setter feedback feedBack = feedback.normal;
+
+	@ManyToOne
 	private @Getter @Setter Task task;
+	
+	@Column
+	private @Getter @Setter boolean isDelayed = false;
+	
+	@Column
+	private @Getter @Setter boolean isBreaked = false;
 }
--- whereIsTime-repo-service/src/main/java/com/whereIsTime/entities/Task.java
+++ whereIsTime-repo-service/src/main/java/com/whereIsTime/entities/Task.java
@@ -1,12 +1,15 @@
 package com.whereIsTime.entities;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -17,63 +20,73 @@ import lombok.Setter;
 @Table(name = "Task")
 @Entity
 public class Task extends baseEntity {
-	
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 7481555109907855964L;
-	
+
 	public static enum Status {
 		LIVE, COMPLETED;
 	}
-	
-	public Task() {super();}
-	
+
+	public Task() {
+		super();
+	}
+
 	@Column(nullable = false)
 	private @Getter @Setter Status status = Status.LIVE;
-	
+
 	@Column(nullable = false, unique = true)
 	private @Getter @Setter String name;
-	
+
 	@Column
 	private @Getter @Setter String description = "";
-	
-	@Column(nullable = false)
-	private @Getter @Setter Date beginTime;
 
-	@Column(nullable = false)
-	private @Getter @Setter Date endTime;
-	
-	@Column(nullable = false)
-	private @Getter @Setter Date planBeginTime;
+	@Column
+	private @Getter @Setter Date beginTime = null;
 
-	@Column(nullable = false)
-	private @Getter @Setter Date planEndTime;
-	
-	
-	@ManyToOne(cascade = CascadeType.ALL)
-	private @Getter @Setter Catalog catalog;
-	
-	@ManyToOne(cascade = CascadeType.ALL)
+	@Column
+	private @Getter @Setter Date endTime = null;
+
+	@ManyToOne
 	private @Getter @Setter User user;
+
+	@OneToMany(mappedBy = "task", cascade = CascadeType.REMOVE)
+	private @Getter @Setter List<TaskItem> taskItems = new ArrayList<TaskItem>();
+
+	@OneToMany(mappedBy = "task", cascade = CascadeType.REMOVE)
+	private @Getter @Setter List<Mtomato> mtomatos = new ArrayList<Mtomato>();
+
+	@ManyToMany
+	private @Getter @Setter List<Classification> classifications = new ArrayList<Classification>();
 	
-	
-	@OneToMany(mappedBy = "task",
-			   cascade = CascadeType.REMOVE)
-	private @Getter @Setter Set<TaskItem> taskItems = new HashSet<TaskItem>();
-	
-	
-	@OneToMany(mappedBy = "task",
-			   cascade = CascadeType.REMOVE)
-	private @Getter @Setter Set<Mtomato> mtomatos = new HashSet<Mtomato>();
-	
-	/*fetch before call*/
+	/**
+	 * 使用之前需要确保taskItems已经取到
+	 * 
+	 * @return 任务的子项是否全部完成
+	 * @see com.whereIsTime.Services.TaskService#getTask(java.lang.Long)
+	 */
 	public boolean allComplete() {
-		for (TaskItem item: taskItems) {
+		if (taskItems == null)
+			return false;
+		for (TaskItem item : taskItems) {
 			if (item.getStatus() != TaskItem.Status.COMPLETED) {
 				return false;
 			}
 		}
 		return true;
+	}
+
+	public void addTaskItem(TaskItem item) {
+		taskItems.add(item);
+	}
+
+	public void addMtomatos(Mtomato m) {
+		mtomatos.add(m);
+	}
+	
+	public void addClassification(Classification c) {
+		classifications.add(c);
 	}
 }
